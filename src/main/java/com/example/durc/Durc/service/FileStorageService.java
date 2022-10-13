4061c002d23b23195dc1caaf0605fd24b3396f98
Shadman <shadmaan.sobhani@gmail.com>
2022-10-13 17:55:34 +0600

    This is my code. I did this for testing.

--- src/main/java/com/example/durc/Durc/service/FileStorageService.java
+++ src/main/java/com/example/durc/Durc/service/FileStorageService.java
@@ -2,8 +2,12 @@ package com.example.durc.Durc.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface FileStorageService {
-    String storeFile(MultipartFile file);
 
-
+    public String uploadImage(MultipartFile file) throws IOException;
+    public byte[] downloadImage(String file_name);
+    public String uploadImageToTheFileSystem(MultipartFile file) throws IOException;
+    public byte[] downloadImageFromTheFileSystem(String file_name);
 }
--- src/main/java/com/example/durc/Durc/service/impl/FileStorageServiceImpl.java
+++ src/main/java/com/example/durc/Durc/service/impl/FileStorageServiceImpl.java
@@ -1,7 +1,11 @@
 package com.example.durc.Durc.service.impl;
 
+import com.example.durc.Durc.entity.ImageData;
+import com.example.durc.Durc.repository.StorageRepository;
 import com.example.durc.Durc.service.FileStorageService;
+import com.example.durc.Durc.util.ImageUtil;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,25 +14,61 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 @Service
 public class FileStorageServiceImpl implements FileStorageService {
 
-    Path fileStoragePath;
 
-    public FileStorageServiceImpl(@Value("${file.storage.location:temp}") String fileStorageLocation ) {
-        fileStoragePath = Paths.get(fileStorageLocation).toAbsolutePath().normalize();
+    @Autowired
+    private StorageRepository storageRepository;
 
-        try {
-            Files.createDirectories(fileStoragePath);
-        } catch (IOException e) {
-            throw new RuntimeException("Issue in creating file directory.");
-        }
+    @Override
+    public String uploadImage(MultipartFile file) throws IOException {
+
+        ImageData imageData = storageRepository.save(ImageData.builder()
+                .name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .imageData(ImageUtil.compressImage(file.getBytes())).build()
+        );
+
+        if (imageData != null) {
+
+            return "File was uploaded successfully: " + file.getOriginalFilename();
+        } else
+            return "An error occurred while uploading" + file.getOriginalFilename();
     }
 
     @Override
-    public String storeFile(MultipartFile file) {
-        return null;
+    public byte[] downloadImage(String file_name) {
+
+        Optional<ImageData> dbImageData = storageRepository.findByName(file_name);
+        byte[] images = ImageUtil.decompressImage(dbImageData.get().getImageData());
+        return images;
+    }
+
+    @Override
+    public String uploadImageToTheFileSystem(MultipartFile file) throws IOException {
+
+        ImageData imageData = storageRepository.save(ImageData.builder()
+                .name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .imageData(ImageUtil.compressImage(file.getBytes())).build()
+        );
+
+        if (imageData != null) {
+
+            return "File was uploaded successfully: " + file.getOriginalFilename();
+        } else
+            return "An error occurred while uploading" + file.getOriginalFilename();
+    }
+
+    @Override
+    public byte[] downloadImageFromTheFileSystem(String file_name) {
+
+        Optional<ImageData> dbImageData = storageRepository.findByName(file_name);
+        byte[] images = ImageUtil.decompressImage(dbImageData.get().getImageData());
+        return images;
 
     }
 }
